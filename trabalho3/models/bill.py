e317c2d156abcdd5cf23651ce861df4d435edf0d
Amedeo Elmo <elmoamedeo@gmail.com>
2020-05-16 18:53:07 -0300

    Corrigindo migrations

--- trabalho3/models/bill.py
+++ trabalho3/models/bill.py
@@ -4,8 +4,8 @@ from ..models import costumer, provider
 
 class Bill(models.Model):
     price = models.CharField(max_length=10)
-    costumer = models.ForeignKey(costumer.Costumer, on_delete=models.CASCADE)
-    provider = models.ForeignKey(provider.Provider, on_delete=models.CASCADE)
+    costumer = models.ForeignKey(costumer.Costumer, on_delete=models.CASCADE, default='null')
+    provider = models.ForeignKey(provider.Provider, on_delete=models.CASCADE, default='null')
 
     class Meta:
         db_table = "bill"
