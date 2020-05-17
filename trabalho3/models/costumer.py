e9868ee2a913bc35a6ce5210f91638043f0a6af8
Amedeo Elmo <elmoamedeo@gmail.com>
2020-05-17 04:50:24 -0300

    Costumer fix

--- trabalho3/models/costumer.py
+++ trabalho3/models/costumer.py
@@ -5,7 +5,7 @@ class Costumer(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     neighborhood = models.CharField(max_length=100)
-    address_number = models.IntegerField(max_length=10)
+    address_number = models.IntegerField()
     email = models.EmailField()
     number = models.CharField(max_length=15)
 
