ad5f32b3b405788ad1fcc37f2b03ee3e1de1b752
Amedeo Elmo <elmoamedeo@gmail.com>
2020-05-17 02:13:33 -0300

    Ajustes nos forms HTML e nova migration

--- trabalho3/models/product.py
+++ trabalho3/models/product.py
@@ -1,9 +1,11 @@
 from django.db import models
+from ..models import provider
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    quantity = models.CharField(max_length=5)
+    quantity = models.IntegerField(max_length=5)
+    provider = models.ForeignKey(provider.Provider, on_delete=models.CASCADE, default='null')
 
     class Meta:
         db_table = "product"
